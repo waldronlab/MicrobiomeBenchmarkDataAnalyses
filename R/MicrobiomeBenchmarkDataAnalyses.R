429d18e12c0e4345113e668fc6e7295a8ed20634
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-02-15 17:48:59 -0500

    Add package description

---
+++ R/MicrobiomeBenchmarkDataAnalyses.R
@@ -0,0 +1,11 @@
+
+#' MicrobiomeBenchmarkDataAnalyses
+#' 
+#' \code{MicrobiomeBenchmarkDataAnalyses} is a package for the DA analyses
+#' performed on the MicrobiomeBenchmarkData package.
+#' 
+#' @docType package
+#' @name MicrobiomeBenchmarkDataAnalyses
+#' @importFrom magrittr %>%
+#' 
+NULL
