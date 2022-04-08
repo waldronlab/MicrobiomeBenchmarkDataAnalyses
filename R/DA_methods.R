a37d33ffe3f04c0e4dd6f0e77d21c3fef7554019
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-04-08 16:23:54 -0400

    Create wrappers for setNormalizations and set_DA_methods

--- R/DA_methods.R
+++ R/DA_methods.R
@@ -1,104 +1,141 @@
 
 #' Set list of DA methods
 #' 
-#' \code{set_DA_methods_list} creates a list of methods for the benchdamic
-#' workflow.
+#' \code{set_DA_methods_list} creates a predefined list of methods for the
+#' benchdamic workflow. This should be input to the
+#' \code{\link[benchdamic]{runDA}} function.
 #'
-#' @param grp Character string. Name of the column
-#' @param contrast Character vector. Reference goes first.
+#' @param conditions_col Character string indicating the name of the column
+#' containing the conditions of the samples in sample_metadata/colData. 
+#' 
+#' @param conditions A named character vector with names condB and condA. 
+#' Reference is condB. For example, `c(condB = 'control', condA = 'treatment')`.
 #'
-#' @return A list of methods for benchdamic
+#' @return A list of DA methods for benchdamic.
 #' @export
 #'
-set_DA_methods_list <- function(grp, contrast) {
+set_DA_methods_list <- function(conditions_col, conditions) {
     
-    # edgeR
-    my_edger <- benchdamic::set_edgeR(
-        group_name = grp,
-        design = as.formula(paste0("~", grp)),
-        norm = "TMM",
-        coef = 2
-    )
-    
-    # DESeq2
-    my_deseq2 <- benchdamic::set_DESeq2(
-        contrast = c(grp, contrast[2], contrast[1]),
-        design = as.formula(paste0("~", grp)),
-        norm = "poscounts"
-    )
-    
-    # limma 
-    my_limma <- benchdamic::set_limma( # I get a warning
-        design = as.formula(paste0("~", grp)),
-        norm = c("TMM", "CSSmedian"),
-        coef = 2
-    )
-    
-    # metagenomeSeq
-    my_metagenomeseq <- benchdamic::set_metagenomeSeq(
-        design = as.formula(paste0("~", grp)),
-        norm = "CSSmedian",
-        coef = 2
-    )
-    
-    # ALDEx2
-    my_aldex2 <- benchdamic::set_ALDEx2(
-        conditions = grp,
-        test = "t",
-        norm = "none"
-    )
-    
-    # corncob
-    my_corncob <- benchdamic::set_corncob(
-        formula = as.formula(paste0("~", grp)),
-        phi.formula = as.formula(paste0("~", grp)),
-        formula_null = ~ 1,
-        phi.formula_null = as.formula(paste0("~", grp)),
-        test = "Wald",
-        coefficient = paste0(grp, contrast[2]),
-        norm = "none"
-    )
-    
-    # MAST
-    my_mast <- benchdamic::set_MAST(
-        rescale = "median",
-        design = as.formula(paste0("~", grp)),
-        coefficient = paste0(grp, contrast[2]),
-        norm = "none"
-    )
-    
-    # Seurat
-    my_seurat <- benchdamic::set_Seurat(
-        test.use = "wilcox",
-        contrast = c(grp, contrast[2], contrast[1]),
-        norm = "none"
-    )
-    
-    my_new_methods <- list(
-        zinq.1 = list(method = "zinq", grp = grp, ref = contrast[2]),
-        ancombc.2 = list(method = "ancombc", formula = grp, group = grp),
-        wilcox_test.3 = list(
-            method = "DA_wilcox_test", grp = grp, ref = contrast[2]
+    c(
+        # edgeR
+        my_edger <- benchdamic::set_edgeR(
+            group_name = conditions_col,
+            design = as.formula(paste0("~", conditions_col)),
+            norm = "TMM",
+            coef = 2,
+            weights_logical = FALSE
         ),
-        wilcox_test_clr.4 = list(
-            method = "DA_wilcox_test_clr", grp = grp, ref = contrast[2]
+        
+        # edgeR + weights
+        
+        my_edger <- benchdamic::set_edgeR(
+            group_name = conditions_col,
+            design = as.formula(paste0("~", conditions_col)),
+            norm = "TMM",
+            coef = 2,
+            weights_logical = TRUE
         ),
-        kruskal_test.5 = list(
-            method = "DA_kruskal_test", grp = grp, ref = contrast[2]
+        
+        
+        # DESeq2
+        my_deseq2 <- benchdamic::set_DESeq2(
+            contrast = c(conditions_col, conditions['condA'], conditions['condB']),
+            design = as.formula(paste0("~", conditions_col)),
+            norm = "poscounts",
+            weights_logical = FALSE
         ),
-        kruskal_test_clr.6 = list(
-            method = "DA_kruskal_test_clr", grp = grp, ref = contrast[2]
+        
+        # DESeq2 + weights
+        my_deseq2 <- benchdamic::set_DESeq2(
+            contrast = c(conditions_col, conditions['condA'], conditions['condB']),
+            design = as.formula(paste0("~", conditions_col)),
+            norm = "poscounts",
+            weights_logical = TRUE
         ),
-        lefse.7= list(
-            method = "DA_lefse", grp = grp, ref = contrast[1]
+        
+        # limma 
+        my_limma <- benchdamic::set_limma( # I get a warning
+            design = as.formula(paste0("~", conditions_col)),
+            norm = "TMM",
+            coef = 2,
+            weights_logical = FALSE
+        ),
+        
+        # limma + weights 
+        my_limma <- benchdamic::set_limma( # I get a warning
+            design = as.formula(paste0("~", conditions_col)),
+            norm = "TMM", 
+            coef = 2,
+            weights_logical = TRUE
+        ),
+        
+        # metagenomeSeq
+        my_metagenomeseq <- benchdamic::set_metagenomeSeq(
+            design = as.formula(paste0("~", conditions_col)),
+            norm = c("CSSmedian", "CSSdefault"),
+            coef = 2
+        ),
+        
+        # ALDEx2 with t-test
+        my_aldex2 <- benchdamic::set_ALDEx2(
+            conditions = conditions_col,
+            test = "t",
+            norm = "none"
+        ),
+        
+        # ALDEx2 with wilcox
+        my_aldex2 <- benchdamic::set_ALDEx2(
+            conditions = conditions_col,
+            test = "wilcox",
+            norm = "none"
+        ),
+        
+        # corncob
+        my_corncob <- benchdamic::set_corncob(
+            formula = as.formula(paste0("~", conditions_col)),
+            phi.formula = as.formula(paste0("~", conditions_col)),
+            formula_null = ~ 1,
+            phi.formula_null = as.formula(paste0("~", conditions_col)),
+            test = "Wald",
+            coefficient = paste0(conditions_col, conditions['condA']),
+            norm = "none"
+        ),
+        
+        # MAST
+        my_mast <- benchdamic::set_MAST(
+            rescale = "median",
+            design = as.formula(paste0("~", conditions_col)),
+            coefficient = paste0(conditions_col, conditions['condA']),
+            norm = "none"
+        ),
+        
+        # Seurat
+        my_seurat <- benchdamic::set_Seurat(
+            test.use = "wilcox",
+            contrast = c(conditions_col, conditions['condA'], conditions['condB']),
+            norm = "none"
         )
         
+        # my_new_methods <- list(
+        #     zinq.1 = list(method = "zinq", grp = grp, ref = contrast[2]),
+        #     ancombc.2 = list(method = "ancombc", formula = grp, group = grp),
+        #     wilcox_test.3 = list(
+        #         method = "DA_wilcox_test", grp = grp, ref = contrast[2]
+        #     ),
+        #     wilcox_test_clr.4 = list(
+        #         method = "DA_wilcox_test_clr", grp = grp, ref = contrast[2]
+        #     ),
+        #     kruskal_test.5 = list(
+        #         method = "DA_kruskal_test", grp = grp, ref = contrast[2]
+        #     ),
+        #     kruskal_test_clr.6 = list(
+        #         method = "DA_kruskal_test_clr", grp = grp, ref = contrast[2]
+        #     ),
+        #     lefse.7= list(
+        #         method = "DA_lefse", grp = grp, ref = contrast[1]
+        #     )
+        #     
+        # )
     )
-    
-    my_methods <- c(
-        my_edger, my_deseq2, my_limma, my_metagenomeseq, my_aldex2,
-        my_corncob, my_mast, my_seurat, my_new_methods
-    )
-    
-    my_methods
+
 }
--- R/normalization_methods.R
+++ R/normalization_methods.R
@@ -1,30 +1,40 @@
 
-#' Normalization methods
-normalization_methods <- tibble::tribble(
-    ~fun, ~method,
-    # "norm_CSS", "default",
-    "norm_CSS", "median",
-    "norm_edgeR", "TMM",
-    # "norm_edgeR", "TMMwsp",
-    # "norm_edgeR", "RLE", # This is the same as poscounts?
-    # "norm_edgeR", "upperquartile",
-    # "norm_edgeR", "posupperquartile",
-    "norm_DESeq2", "poscounts", # RLE
-    # "norm_DESeq2", "ratio",
-    # "norm_DESeq2", "iterate",
-    "norm_TSS", "TSS",
-    "norm_edgeR", "none"
-)
-
-#' set normalization
+#' Set list of normalization methods
 #' 
-#' @importFrom benchdamic setNormalizations
+#' \code{set_norm_list} is a wrapper of the 
+#' \code{\link[benchdamic]{setNormalizations}} function with already predefined
+#' normalization methods. The methods are: CSSdefault, CSSmedian, TMM,
+#' poscounts, and none.
+#' 
+#' The output should be used as input of the
+#' \code{\link[benchdamic]{runNormalizations}} function.
+#' 
+#' 
+#' @return A list of normalization methods compatible with the benchdamic
+#' framework.
 #' 
 #' @export
-normalization_methods_list <- benchdamic::setNormalizations(
-    fun = normalization_methods$fun, 
-    method = normalization_methods$method
-)
-
-
-
+#' 
+set_norm_list <- function() {
+    
+    normalization_methods <- tibble::tribble(
+        ~fun, ~method,
+        "norm_CSS", "default",
+        "norm_CSS", "median",
+        "norm_edgeR", "TMM",
+        # "norm_edgeR", "TMMwsp",
+        # "norm_edgeR", "RLE",
+        # "norm_edgeR", "upperquartile",
+        # "norm_edgeR", "posupperquartile",
+        "norm_DESeq2", "poscounts", # RLE
+        # "norm_DESeq2", "ratio",
+        # "norm_DESeq2", "iterate",
+        # "norm_TSS", "TSS",
+        "norm_edgeR", "none"
+    )
+    
+    benchdamic::setNormalizations(
+        fun = normalization_methods$fun,
+        method = normalization_methods$method
+    )
+}
