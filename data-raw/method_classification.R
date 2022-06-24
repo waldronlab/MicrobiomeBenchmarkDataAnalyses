be1394ee6561a5a0df4f2d9b30d00cfca7cb3716
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-06-24 16:07:51 -0400

    Change import file for methods_classification.

--- data-raw/method_classification.R
+++ data-raw/method_classification.R
@@ -1,8 +1,14 @@
 ## code to prepare `method_classification` dataset goes here
 
 url <- 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTrwtp0Br2q79Iq_Ar1rQeq3cll4aab0l6lkRBqcX0I7EbWnunRkQVkIYUG58cKlYfoSFY4UCPYUdef/pub?gid=0&single=true&output=tsv'
+
+fname <- system.file(
+    'extdata/method_classification.tsv',
+    package = 'MicrobiomeBenchmarkDataAnalyses'
+)
+
 methods_classification <- read.table(
-    file = url, header = TRUE, sep = '\t', row.names = NULL
+    file = fname, header = TRUE, sep = '\t', row.names = NULL
 )
 
 
