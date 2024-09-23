c4c8e5a5b5ba6f64b1fb7db0c3da585c4ce6faf1
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-09-23 02:17:07 -0400

    Update documentation and DESCRIPTION file with a newer version of roxygen

--- R/MicrobiomeBenchmarkDataAnalyses.R
+++ R/MicrobiomeBenchmarkDataAnalyses.R
@@ -4,7 +4,6 @@
 #' \code{MicrobiomeBenchmarkDataAnalyses} is a package for the DA analyses
 #' performed on the MicrobiomeBenchmarkData package.
 #' 
-#' @docType package
 #' @name MicrobiomeBenchmarkDataAnalyses
 #' @importFrom magrittr %>%
 #' 
